2d980bb6d837540152a9245da27c035a6258c8a1
Thales Sousa <thalessousa@thaless-mbp.home>
2018-07-31 21:26:33 +0200

    adding zuul routes

--- station/src/main/kotlin/com/thalesminussi/station/controller/WeatherController.kt
+++ station/src/main/kotlin/com/thalesminussi/station/controller/WeatherController.kt
@@ -3,10 +3,7 @@ package com.thalesminussi.station.controller
 import com.thalesminussi.station.model.WeatherResponseDto
 import com.thalesminussi.station.service.ForecastService
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/")
@@ -16,4 +13,5 @@ class WeatherController(val forecastService: ForecastService) {
     fun getWeatherByCity(@RequestParam city: String, @RequestParam(required = false) units: String?): WeatherResponseDto {
         return this.forecastService.getWeatherByCity(city, units)
     }
+
 }
--- station/src/main/kotlin/com/thalesminussi/station/rest/OpenWeatherMap.kt
+++ station/src/main/kotlin/com/thalesminussi/station/rest/OpenWeatherMap.kt
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestParam
 interface OpenWeatherMap {
 
     @GetMapping
-    fun getWeatherByCity(@RequestParam q: String, @RequestParam(required = false) unit: String?, @RequestParam appid: String): WeatherResponseDto
+    fun getWeatherByCity(@RequestParam q: String, @RequestParam(required = false) units: String?, @RequestParam appid: String): WeatherResponseDto
 
 }
