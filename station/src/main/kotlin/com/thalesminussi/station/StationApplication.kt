cc045c407cb599d36854f51ab5a8ee9bba02655e
Thales Sousa <thalessousa@192.168.2.2>
2018-08-02 22:43:53 +0200

    removing eureka servers from client projects

--- station/src/main/kotlin/com/thalesminussi/station/StationApplication.kt
+++ station/src/main/kotlin/com/thalesminussi/station/StationApplication.kt
@@ -2,11 +2,9 @@ package com.thalesminussi.station
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableFeignClients
 class StationApplication
 
